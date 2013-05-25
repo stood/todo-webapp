f6d05e4a5344e1a9eb197e37d38e42710cc0b165
Sanpi <sanpi@homecomputing.fr>
2013-05-25 16:47:34 +0200

    Allow cross domain request

--- web/js/app.js
+++ web/js/app.js
@@ -2,6 +2,11 @@
 
 var app = angular.module('todo.txt', ['tasksServices']);
 
+app.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);
+
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/tasks', {
         templateUrl: 'partials/tasks.html',
