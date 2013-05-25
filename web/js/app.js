4c55efed20cfecd589949b5944fb0e13a1e07709
Sanpi <sanpi@homecomputing.fr>
2013-05-25 16:46:18 +0200

    More readable app.js

--- web/js/app.js
+++ web/js/app.js
@@ -1,15 +1,15 @@
 'use strict';
 
-angular.module('todo.txt', ['tasksServices'])
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider
-            .when('/tasks', {
-                templateUrl: 'partials/tasks.html',
-                controller: TasksListController
-            })
-            .when('/tasks/:taskId', {
-                templateUrl: 'partials/task.html',
-                controller: TaskController
-            })
-            .otherwise({redirectTo: '/tasks'});
-    }]);
+var app = angular.module('todo.txt', ['tasksServices']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/tasks', {
+        templateUrl: 'partials/tasks.html',
+        controller: TasksListController
+    });
+    $routeProvider.when('/tasks/:taskId', {
+        templateUrl: 'partials/task.html',
+        controller: TaskController
+    });
+    $routeProvider.otherwise({redirectTo: '/tasks'});
+}]);
