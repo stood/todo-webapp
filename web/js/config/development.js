7fcd395884899615775f25276f0e4a33102f1cbc
Sanpi <sanpi@homecomputing.fr>
2013-05-25 17:49:11 +0200

    Configuration API end point

---
+++ web/js/config/development.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('config', [])
+    .constant('API_END_POINT', 'localhost:8080');
+
--- web/js/services.js
+++ web/js/services.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('tasksServices', ['ngResource'])
-    .factory('Tasks', function ($resource) {
-        return $resource('api/tasks/:taskId');
+angular.module('tasksServices', ['ngResource', 'config'])
+    .factory('Tasks', function ($resource, API_END_POINT) {
+        return $resource('http://:api_end_point/tasks/:taskId', {api_end_point: API_END_POINT});
     });
