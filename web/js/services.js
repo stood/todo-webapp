a035bc1cc2aa5a9cd386c306a9e7093719df3106
Sanpi <sanpi@homecomputing.fr>
2013-11-07 00:16:23 +0100

    Add missing taskId resource parameter

--- web/js/services.js
+++ web/js/services.js
@@ -2,5 +2,8 @@
 
 angular.module('tasksServices', ['ngResource', 'config'])
     .factory('Tasks', function ($resource, API_END_POINT) {
-        return $resource('http://:api_end_point/tasks/:taskId', {api_end_point: API_END_POINT});
+        return $resource(
+            'http://:api_end_point/tasks/:taskId',
+            {api_end_point: API_END_POINT, taskId: '@taskId'}
+        );
     });
