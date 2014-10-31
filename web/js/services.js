631dbee1222443c0d8f135969cad72a4c7df18af
Sanpi <sanpi@homecomputing.fr>
2014-10-31 15:52:49 +0100

    Fixed mixed content

--- web/js/services.js
+++ web/js/services.js
@@ -3,13 +3,13 @@
 angular.module('tasksServices', ['ngResource', 'config'])
     .factory('Tasks', function ($resource, API_END_POINT) {
         return $resource(
-            'http://:api_end_point/tasks/:taskId',
+            '//:api_end_point/tasks/:taskId',
             {api_end_point: API_END_POINT, taskId: '@taskId'},
             {
                 'save': { method: 'POST' },
                 'update': { method: 'PUT' },
-                'complete': { method: 'POST', url: 'http://:api_end_point/tasks/:taskId/complete' },
-                'uncomplete': { method: 'POST', url: 'http://:api_end_point/tasks/:taskId/uncomplete' }
+                'complete': { method: 'POST', url: '//:api_end_point/tasks/:taskId/complete' },
+                'uncomplete': { method: 'POST', url: '//:api_end_point/tasks/:taskId/uncomplete' }
             }
         );
     })
